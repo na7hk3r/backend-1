3082a38d17a2a4d5d63c70bf70cb99ec4133d903
Mathias Curbelo <104522465+na7hk3r@users.noreply.github.com>
2024-04-25 19:37:42 -0300

    Ejercitacion 1 Test JUnit

---
+++ src/Persona.java
@@ -0,0 +1,55 @@
+public class Persona {
+
+    private String name;
+    private String surname;
+    private String email;
+    private int age;
+
+    public Persona(String name, String surname, String email, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // Metodos
+    public boolean isAdult(){
+        return this.age > 18;
+    }
+
+    public String fullName(){
+        return this.surname + ", " + this.name;
+    }
+}
---
+++ src/PersonaTest.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonaTest {
+
+    @Test
+    void testIsAdult() {
+        // "Fecha actual"
+        LocalDate fechaActual = LocalDate.of(2024,9, 21);
+
+        // Datos de Prueba
+        LocalDate fechaNacimientoMenor = LocalDate.of(2015, 1, 20);
+        LocalDate fechaNacimientoMayor = LocalDate.of(2000, 3, 10);
+
+        int caso1 = calcularEdad(fechaActual, fechaNacimientoMenor);
+        int caso2 = calcularEdad(fechaActual, fechaNacimientoMayor);
+
+        assertFalse(isAdult(caso1));
+        assertTrue(isAdult(caso2));
+
+        // assertFalse(isAdult(caso2)); // True
+        // assertTrue(isAdult(caso1)); // False
+
+    }
+
+    private int calcularEdad(LocalDate fechaActual, LocalDate fechaNacimiento) {
+        return fechaActual.getYear() - fechaNacimiento.getYear();
+    }
+    private boolean isAdult(int age) {
+        return age >= 18;
+    }
+
+
+    @Test
+    void testFullName() {
+        // Datos de Prueba
+        Persona persona1 = new Persona("Walter", "Dos Santos", "chinela10@mail.com",64);
+
+        assertEquals("Dos Santos, Walter", persona1.fullName());
+
+        // assertEquals("Walter Dos Santos", persona1.fullName());
+    }
+}
