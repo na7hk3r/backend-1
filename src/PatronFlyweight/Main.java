97ac4deef7ca3dc74b215c4a895283781b69c560
Mathias Curbelo <104522465+na7hk3r@users.noreply.github.com>
2024-05-02 17:49:58 -0300

    Ejercitación Patron Flyweight

---
+++ src/PatronFlyweight/Main.java
@@ -0,0 +1,18 @@
+package PatronFlyweight;
+
+import PatronFlyweight.ComidaDTO;
+import PatronFlyweight.ComidaFactory;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        final ComidaFactory comida = new ComidaFactory();
+
+        ComidaDTO pastaVerdura = comida.getComida("Pastas");
+        pastaVerdura.setPrecio(10000);
+
+        final ComidaDTO pastaCarne = comida.getComida("Pastas");
+        System.out.println(pastaCarne.toString());
+    }
+}
